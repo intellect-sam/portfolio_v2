578c4c870cbf7ae469fc16fcd0695de9c415a876
intellect-sam <alukoolakunlesamuel@gmail.com>
2023-09-04 21:01:58 +0100

    reverve hero section

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 import { Navbar, Hero, About } from './components';
 
 const App = () => (
-  <div className="px-4 overflow-hidden">
+  <div className="overflow-hidden">
     <div className="bg-[#030712] border-b-4 shadow-md border-black">
-      <div className="sticky top-0">
+      <div className="sticky top-0 px-4">
         <Navbar />
       </div>
     </div>
@@ -13,12 +13,14 @@ const App = () => (
       <Home/>
     </div> */}
 
-    <div className="min-h-screen md:w-full">
-      <Hero />
-    </div>
+    <div className="px-4">
+      <div className="min-h-screen md:w-full">
+        <Hero />
+      </div>
 
-    <div className="min-h-screen md:w-full ">
-      <About />
+      <div className="min-h-screen md:w-full ">
+        <About />
+      </div>
     </div>
 
     {/* <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
